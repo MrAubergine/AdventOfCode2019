d46b555237d08cbd4f7136301711ed76f1183b1f
MrAubergine <chris@jackson.me.uk>
2019-12-09 23:26:08 +0000

    Day 9 done

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace AdventOfCode2019
     {
         static void Main(string[] args)
         {
-            IDay Day = new Day8();
+            IDay Day = new Day9();
 
             Day.Part1();
             Day.Part2();
